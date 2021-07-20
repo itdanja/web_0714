c887c39c616ed832d8cf0ee3d02ad8fd85b237f8
itdanja <itdanja@kakao.com>
2021-07-20 18:52:14 +0900

    day05

--- java1/src/Day05/day05_2.java
+++ java1/src/Day05/day05_2.java
@@ -62,12 +62,55 @@ System.out.println("\n------------------------------");
 			System.out.println();
 			
 		}
+System.out.println("\n------------------------------");				
+		System.out.print("줄7 개수 : "); int 줄7 = scanner.nextInt();
+		for( int i = 1 ; i<=줄7 ; i++ ) {
+			// 공백찍기
+			for(int b = 1 ; b<=줄7-i; b++) {System.out.print(" ");}
+			// 별찍기 
+			for(int s = 1 ; s<=i*2-1; s++ ) {System.out.print("*");}
+			// 줄바꿈
+			System.out.println();
+			
+		}
+System.out.println("\n------------------------------");				
+		System.out.print("줄8 개수 : "); int 줄8 = scanner.nextInt();
+		for( int i = 1 ; i<=줄8 ; i++ ) {
+			// 공백찍기
+			for(int b = 1 ; b<=줄8-i; b++) {System.out.print(" ");}
+			// 별찍기 
+			for(int s = 1 ; s<=i*2-1; s++ ) {System.out.print(i);}
+			// 줄바꿈
+			System.out.println();
+			
+		}		
+System.out.println("\n------------------------------");				
+	System.out.print("줄9 개수 : "); int 줄9 = scanner.nextInt();
+	for( int i = 1 ; i<=줄9 ; i++ ) {
+		// 공백찍기
+		for(int b = 1 ; b<=i-1; b++) {System.out.print(" ");}
+		// 별찍기 
+		for(int s = 1 ; s<=줄9*2-(i*2-1); s++ ) {System.out.print("*");}
+		// 줄바꿈
+		System.out.println();
+	}		
+
+System.out.println("\n------------------------------");	
+	System.out.print("줄11 개수 : "); int 줄11 = scanner.nextInt();
+	for( int i = 1 ; i<=줄11 ; i++ ) { // i = 행 = 가로단위
 		
+		for( int j = 1 ; j<=줄11 ; j++ ) {
+			// 조건1 : 행/열 만나는 지점 
+			if( i == j )System.out.print("*");
+			else if( (i+j) ==(줄11+1) ) System.out.print("*"); // 조건2 : 
+			else System.out.print(" ");
+		}
+		// 줄바꿈
+		System.out.println();
 		
-		
-		
-		
-		
+	}
+	
+			
 		
 		
 		
