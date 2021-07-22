476372772fdfbc8fe1ddcd6ae9a1e3e9626d5709
itdanja <itdanja@kakao.com>
2021-07-22 18:31:13 +0900

    day07

---
+++ java1/src/Day05/day05_과제결과.java
@@ -0,0 +1,100 @@
+package Day05;
+
+public class day05_과제결과 {
+
+
+
+
+}
+
+
+// 별문제12
+/*
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int N = scan.nextInt();
+  int col = 0;
+  int space = 2;
+  
+  for(int i=1; i<=N; i++) {
+     for(int j=0; j<i; j++)
+        System.out.print("*");
+     
+     for(int j=(i-1)*2; j<(N*2)-2; j++)
+        System.out.print(" ");
+     for(int j=0; j<i; j++)
+        System.out.print("*");
+     
+     System.out.println();
+  }
+  
+  for(int i=N; i<(N*2)-1; i++) {
+     for(int j=col; j<N-1; j++)
+        System.out.print("*");
+     
+     for(int j=0; j<space; j++)
+        System.out.print(" ");
+     
+     for(int j=col; j<N-1; j++)
+        System.out.print("*");
+     
+     System.out.println();
+     col ++;
+     space += 2;
+  }
+  scan.close();
+}
+}
+*/
+
+
+
+
+//별문제13
+/*
+import java.util.Scanner;
+
+public class Main {
+ 
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+     
+    String map[] = new String[n];
+    map[0] = "  *  ";
+    map[1] = " * * ";
+    map[2] = "*****";
+     
+    for (int k = 1; 3 * (int)Math.pow(2, k) <= n; ++k) {
+        makeBigStar(k, map);
+    }
+     
+    for (int i = 0; i < n; ++i) {
+        System.out.println(map[i]);
+    }
+}
+ 
+	private static void makeBigStar(int k, String map[]) {
+	    int bottom = 3 * (int)Math.pow(2, k);
+	    int middle = bottom / 2;
+	     
+	    for (int i = middle; i < bottom; ++i) {
+	        map[i] = map[i - middle] + " " + map[i -middle];
+	    }
+	     
+	    String space = "";
+	    while (space.length() < middle) {
+	        space += " ";
+	    }
+	    for (int i = 0; i < middle; ++i) {
+	        map[i] = space + map[i] + space;
+	    }
+	}
+	}
+
+
+}
+	*/
---
+++ java1/src/Day07/Day07_회원제프로그램.java
@@ -0,0 +1,110 @@
+package Day07;
+
+import java.util.Scanner;
+
+import sun.jvm.hotspot.gc.parallel.PSYoungGen;
+
+public class Day07_회원제프로그램 {
+	
+	// 회원제 프로그램 
+	// 회원가입 · 로그인 
+		// [조건1] 2차원배열 
+		// [조건2] 최대 회원 100명 제한
+			// 1.아이디 , 비밀번호 배열에 저장 
+		// [조건3] 무한루프 [ 초기 메뉴 ]
+			// 1. 회원가입 [중복아이디x]
+			// 2. 로그인 
+			// 3. 종료 
+		// [조건4] 로그인시 메뉴 
+			// 1.회원탈퇴 2.패스워드변경 3.로그아웃[초기메뉴]
+	
+	public static void main(String[] args) {
+		
+		// 0.설정[ 코드상 필요한 객체 ]
+		Scanner scanner = new Scanner(System.in);
+		
+		// 1. 2차원배열 [ 회원100명 아이디 비밀번호 ] 
+			// 100개 행[회원100명] 2개 열[아이디,비밀번호]
+		String[][] memberlist = new String[100][2];
+		
+		// 3. 프로그램 실행 반복문 
+		while(true) { // 무한루프 [ 종료조건 : 3번 입력시 break ] 
+			
+			System.out.println("\n[[[ 회원제 프로그램 ]]]");
+			System.out.print("[[ 1.회원가입 2.로그인 3.종료   선택 : ");
+			int ch = scanner.nextInt();
+			
+			if( ch==1 ) { // 회원가입 
+				
+				System.out.println("[[[ 회원가입 페이지 ]]]");
+				System.out.print("[[ ---> 아이디 : ");	String id = scanner.next();
+				System.out.print("[[ ---> 비밀번호 : ");	String password = scanner.next();
+					// 중복체크 
+					boolean idcheck = true;
+					for( int i = 0 ; i<memberlist.length ;i++) {
+						if( memberlist[i][0] != null && memberlist[i][0].equals(id)) {
+							// 기존회원에 아이디가 null 아니면서 아이디가 앞전에 입력한 아이디와 동일하면 
+							System.out.println("[[실패]] 중복된 아이디가 존재합니다 ");
+							idcheck = false;
+							break; // 가장 가까운 방복문 탈출
+						}
+					}
+					if( idcheck ) {
+						// 회원가입처리 [ 배열에 빈공간을 찾아서 빈공간에 회원넣기]
+						for( int i = 0 ; i<memberlist.length; i++ ) {
+							if( memberlist[i][0] == null ) {
+								memberlist[i][0] = id;
+								memberlist[i][1] = password;
+								System.out.println("[[완료]] : 회원가입이 되었습니다");
+								break;
+							}
+						}
+					}
+			}
+			else if( ch==2 ) { // 로그인 
+				System.out.println("[[[ 로그인 페이지 ]]]");
+				System.out.print("[[ ---> 아이디 : ");		String id = scanner.next();
+				System.out.print("[[ ---> 비밀번호 : ");	String password = scanner.next();
+				
+				// 로그인처리
+				boolean logincheck = true;
+				for( int i = 0 ; i<memberlist.length ; i++ ) {
+									// 2차원배열인 경우
+									// memberlist.length :  행길이
+									// memberlist[0].length : 열길이
+					
+					if( memberlist[i][0] !=null && memberlist[i][0].equals(id) && memberlist[i][1].equals(password) ) {
+						// 로그인성공
+						System.out.println("[["+ id +"]] : 로그인 성공 ");
+						logincheck = false;
+						while(true) { // 로그인시 무한루프 [ 종료조건 : 로그아웃[3] 입력시 ]
+
+							System.out.println("\n[[[ 회원 메뉴 ]]]");
+							System.out.print("[[ 1.회원탈퇴 2.비밀번호변경 3.로그아웃  선택 : ");
+							int ch2 = scanner.nextInt();
+							if( ch2 == 1 ) {}
+							else if( ch2 == 2 ) {}
+							else if( ch2 == 3 ) { System.out.println("[[로그아웃]] 로그아웃 되었습니다"); break;}
+							else {System.out.println("[[알림]] 알수없는 번호 입니다 ");}
+							
+						}
+						break; // 로그아웃 
+					}
+				}
+				if( logincheck ) System.out.println("[[로그인실패]] 동일한 정보가 없습니다 ");
+			}
+			else if( ch==3 ) { System.out.println("[[종료]] 이용해주셔서 감사합니다 "); break;}
+			else {System.out.println("[[알림]] 알수없는 번호 입니다 ");}
+		
+		} // while end 
+		
+		
+		
+		
+		
+		
+		
+	
+		
+	} // main end
+} // class end 
