9156e1958c328be2bf9ef3a5365331b99e44cb11
itdanja <itdanja@kakao.com>
2021-08-02 17:50:32 +0900

    day13

--- java1/src/Day13/Day13_3.java
+++ java1/src/Day13/Day13_3.java
@@ -1,9 +1,11 @@
 package Day13;
 
+import java.io.IOException;
+
 public class Day13_3 {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 	//		도서 검색 프로그램 
 	//		[도서명] : 도서목록 배열에 선언 
@@ -14,6 +16,57 @@ public class Day13_3 {
 		
 //		1. 검색을 입력받아 검색한 단어가 포함된 도서를 모두 출력하기 
 //		2. 검색한 단어를 새로운 교체할 문자를 입력받아 교체 하기 
+		
+		String[] 도서목록 = {	
+				"된다! 네이버 블로그&포스트",
+				"스프링 부트 실전 활용 마스터",
+				"Tucker의 Go 언어 프로그래밍",
+				"혼자 공부하는 C 언어"
+		};
+		
+		byte[] 바이트배열 = new byte[100];
+		System.out.print("검색할 도서명 : ");
+		System.in.read(바이트배열);
+		String 검색어 = new String(바이트배열);
+	
+		// 검색에 해당하는 도서 출력 
+		for( int i = 0 ; i<도서목록.length ;i++ ) {
+			if( 도서목록[i].indexOf(검색어) != -1 ) {
+				System.out.println( 도서목록[i] );
+			}
+		}
+		// 교체
+		System.out.println("교체 단어 : ");
+		System.in.read(바이트배열);
+		String 교체단어 = new String(바이트배열);
+		System.out.println("새로운 단어 : ");
+		System.in.read(바이트배열);
+		String 새로운단어 = new String(바이트배열);
+
+		for( int i = 0 ; i<도서목록.length ; i++  ) {
+			if( 도서목록[i].indexOf(검색어) != -1 ) {
+				도서목록[i].replace( 교체단어 , 새로운단어 );
+			}
+		}
+		// 검색에 해당하는 도서 출력 
+		for( int i = 0 ; i<도서목록.length ;i++ ) {
+			if( 도서목록[i].indexOf(검색어) != -1 ) {
+				System.out.println( 도서목록[i] );
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	
