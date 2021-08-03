45dd57ecee5462909bdcc53f81efbb593c7327a4
itdanja <itdanja@kakao.com>
2021-08-03 18:37:03 +0900

    day14

--- java1/src/Day14/Day14_2.java
+++ java1/src/Day14/Day14_2.java
@@ -1,6 +1,15 @@
 package Day14;
 
+import java.util.Properties;
+import java.util.Scanner;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Day14_2 {
 	
@@ -25,8 +34,101 @@ public class Day14_2 {
 	//				//opens 라이브러리를 사용할패키지명 to activation , mail;
 	//					opens day05 to activation , mail;
 	
+		// 5. 네이버 메일 설정
+				/// 1. 네이버 메일 환경설정
+				// 2. POP3/IMAP 설정
+				// 3. IMAP/SMTP 설정
+				// 4. 사용함  => 확인 
 	
 	public static void main(String[] args) {
-		Session session;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("받는사람 이메일 : ");	String toemail = scanner.next();		// .next() : 공백x 
+		// .nextLine() : 앞에 next() 있는경우 오류 발생 
+			// 대처 방법1 : 사이에 scanner.nextLine(); 추가 
+		scanner.nextLine();
+		System.out.print("메일 제목 : ");		String title = scanner.nextLine();		// .nextLine() : 공백o
+		System.out.print("메일 내용 : ");		String contents = scanner.nextLine();	
+		
+		sendmail(toemail, title, contents);
+		
 	}
+	
+	public static void sendmail( String toemail , String title , String contents ) {
+								// 인수 : 받는사람메일 , 제목 , 내용 
+		//1. 설정 
+		String fromemail="보내는사람이메일@naver.com";
+		String frompassword = "패스워드";
+		
+		Properties properties = new Properties(); 	// 설정관련 [ map 컬렉션 ] 
+		properties.put("mail.smtp.host", "smtp.naver.com");		// host : 메일회사 호스트[ 통신이름] // 메일회사 호스트명[ 해당 사이트내에서 확인가능 ] 
+		properties.put("mail.smtp.port", 587);		// port : 메일회사 호스트 smtp port[통신번호]
+		properties.put("mail.smtp.auth", "true");	// 인증받기 
+		
+		// 2. 인증 
+		//Session session = Session.getDefaultInstance( properties , new Authenticator() { } );
+		Session session = Session.getDefaultInstance( properties , new Authenticator(){ 
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromemail, frompassword);
+			}
+		} );
+		
+		// 3. 메일보내기 
+		try {
+			MimeMessage message = new MimeMessage( session ); // 메시지에 인증 처리 
+			message.setFrom( new InternetAddress( fromemail )); // 보내는사람 // 예외발생 무조건 발생 
+			message.addRecipient( Message.RecipientType.TO , new InternetAddress(toemail ) );
+			
+			message.setSubject(title); // 메일 제목 
+			message.setText(contents); // 메일 내용
+			
+			Transport.send(message); // 메일전송
+			
+			System.out.println("[[[메일전송 완료 ]]] : " + toemail );
+			
+		}
+		catch (Exception e) {
+			System.out.println("[[[메일전송 실패 ]]] : " + toemail );
+		}
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
